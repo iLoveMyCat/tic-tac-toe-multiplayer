099be1119c6345ec220b9594bf08ff5a8b026dc5
Arik Shalito <arikshalito@gmail.com>
2021-10-26 23:29:36 +0300

    populate gamestate

--- public/js/client.js
+++ public/js/client.js
@@ -1,7 +1,33 @@
 var socket = io();
 socket.emit('join server');
 
+
 var userList = document.getElementById('users');
+var board = document.getElementById('board');
+var feedback = document.getElementById('feedback');
+var startBtn = document.getElementById('restart-btn');
+
+document.querySelectorAll('.cell').forEach(tile => {
+    tile.addEventListener('click', event => {
+        socket.emit('click', event.target.id);
+    })
+});
+
+//btn ready
+startBtn.addEventListener('click',()=>{
+    socket.emit('user ready');
+    startBtn.disabled = true;
+    startBtn.innerText = "Ready";
+});
+
+//populate gamestate
+socket.on('game state', ({gameState}) => {
+    console.log(gameState)
+    for(let i = 0; i<gameState.length;i++){  
+        var tile = document.getElementById(i);
+        tile.innerText = gameState[i];
+    }
+});
 
 //populate users on game users event
 socket.on('game users', ({users}) => {
@@ -10,3 +36,12 @@ socket.on('game users', ({users}) => {
     `;
 });
 
+//on start game
+socket.on('start game', () => {
+    console.log("game started");
+});
+
+//waiting
+socket.on('waiting', () => {
+    console.log("waiting for other player");
+});
