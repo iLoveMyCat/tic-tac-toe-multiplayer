a299ca0f0a1ef4a674d0d233cb1f81c0c3a8557d
Arik Shalito <arikshalito@gmail.com>
2021-10-27 16:54:17 +0300

    winning combo functionalities

--- utils/game.js
+++ utils/game.js
@@ -1,14 +1,21 @@
-const gameState = Array(9);
+let gameState = Array(9);
 let gameOver = true;
 let turn = false;
+let winner = undefined;
+const winCombo = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,4,6],[2,5,8],[3,4,5]]; 
 
 function startGame(){
     gameOver = false;
+    gameState = Array(9);
 }
 
 function restartGame(){
     gameState = Array(9);
     gameOver = false;
+}
+
+function setGameOver(over){
+    gameOver = over;
 }
 
 function isGameOver(){
@@ -34,10 +41,53 @@ function isValidMove(idx, team){
     return false;
 }
 
+function isWinner(team){
+    let indexes = getAllIndexes(gameState, team);
+    if(indexes.length >= 3){
+        for(let i = 0; i < winCombo.length; i++){
+            let win = false;
+            for(let j = 0; j < indexes.length; j++){
+                if(winCombo[i].includes(indexes[j])){
+                    win = true;
+                }
+                else{
+                    win = false;
+                    break;
+                }
+            }
+            
+            if(win){
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+function setWinner(team){
+    winner = team;
+}
+
+function getWinner(){
+    return winner;
+}
+
+function getAllIndexes(arr, val) {
+    var indexes = [], i = -1;
+    while ((i = arr.indexOf(val, i+1)) != -1){
+        indexes.push(i);
+    }
+    return indexes;
+}
 
 module.exports = {
     getGameState,
     isGameOver,
+    setGameOver,
     startGame,
     move,
+    isWinner,
+    setWinner,
+    restartGame,
 }
