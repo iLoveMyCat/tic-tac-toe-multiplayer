b66233c3a67f8121e89a686fa9f3d124abcf5166
Arik Shalito <arikshalito@gmail.com>
2021-10-28 13:04:05 +0300

    fixed isWinner

--- utils/game.js
+++ utils/game.js
@@ -2,7 +2,7 @@ let gameState = Array(9);
 let gameOver = true;
 let turn = false;
 let winner = undefined;
-const winCombo = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,4,6],[2,5,8],[3,4,5]]; 
+const winCombo = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,4,6],[2,5,8],[3,4,5],[6,7,8]]; 
 
 function startGame(){
     gameOver = false;
@@ -57,8 +57,8 @@ function isWinner(team){
     if(indexes.length >= 3){
         for(let i = 0; i < winCombo.length; i++){
             let win = false;
-            for(let j = 0; j < indexes.length; j++){
-                if(winCombo[i].includes(indexes[j])){
+            for(let j = 0; j < winCombo[i].length; j++){
+                if(indexes.includes(winCombo[i][j])){
                     win = true;
                 }
                 else{
@@ -72,7 +72,6 @@ function isWinner(team){
             }
         }
     }
-
     return false;
 }
 
